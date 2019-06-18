d7c8bf17ec4bf71a49f3ba27a818e559673e8c0d
Evgeniy <evgeniy.freshlife@gmail.com>
2019-06-18 13:28:08 +0300

    Create Custom Cell

---
+++ LovePlaces/MyCustomCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyCustomCell.swift
+//  LovePlaces
+//
+//  Created by Evgeniy Suprun on 18/06/2019.
+//  Copyright © 2019 Evgeniy Suprun. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var type: UILabel!
+    
+
+}
