4fb4880a4486b4f63c2387e9219bf7ea20847118
Evgeniy <evgeniy.freshlife@gmail.com>
2019-07-24 03:21:03 +0300

    Hello World

--- LovePlaces/AppDelegate.swift
+++ LovePlaces/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+        print("Hello World")
         
         let config = Realm.Configuration(
             // Set the new schema version. This must be greater than the previously used
